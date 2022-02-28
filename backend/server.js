3e19913237013e04b2220a3e8ff02dd9c3bce4f5
Kashif Firoz <firozkashif1234@gmail.com>
2022-02-28 23:18:42 +0530

    routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+const { errorHandler } = require("./middlewares/errorMiddleware");
+const router = require("./routes/goalsRoutes");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// MiddleWares
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/goals", router);
+app.use(errorHandler);
+
+// Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
